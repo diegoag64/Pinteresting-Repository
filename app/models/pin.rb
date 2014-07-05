d65ebf2b5459cc1f9d276d14d79b660ea0e62b01
Diego Arias <diego.arias94@gmail.com>
2014-07-05 13:03:20 -0600

    Still Tryin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@
 class Pin < ActiveRecord::Base
 	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.8.9-Q16'
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :bucket => '******'
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
