500ecb85c381d99318f648a2dec3e51976c58029
Diego Arias <diego.arias94@gmail.com>
2014-07-07 01:37:28 -0600

    Fixed Likes on Signed in

--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -1,8 +1,9 @@
 module PinsHelper
-
 #	def toggle_like_button(pin, user)
 #		if user.flagged?(pin, :like)
 #			link_to "Unlike", like_pin_path(pin)
 #		else
 #			link_to "Like", like_pin_path(pin)
+#		end
+#	end
 end
