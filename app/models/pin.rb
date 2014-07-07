72ca843c85dec586d61fa4ba008c0d5c4edc6ac8
Diego Arias <diego.arias94@gmail.com>
2014-07-06 19:49:04 -0600

    Added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :description, presence: true
+	validates :image, presence: true
+
 end
