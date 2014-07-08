309fe98f78c86180560be0523b0cc93c4af53de8
Diego Arias <diego.arias94@gmail.com>
2014-07-08 01:46:15 -0600

    Trying to fix admin delte

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,10 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!,  except: [:index, :show]
+
+
+
 
   def upvote
     @pin = Pin.find(params[:id])
